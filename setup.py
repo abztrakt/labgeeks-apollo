cba5a6c5754de96da09fef2dc9d2c734eeb30911
Craig M. Stimmel <cstimmel@uw.edu>
2014-06-17 14:36:31 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'labgeeks-apollo',
+    version = '1.0',
+    license = 'Apache',
+    url = 'http://github.com/abztrakt/labgeeks-apollo',
+    description = 'Consultant forms and tools app for the labgeeks suite of student staff management tools.',
+    author = 'Craig Stimmel',
+    packages = ['labgeeks_apollo',],
+    install_requires = [
+        'setuptools',
+        'pillow',
+        'South==0.7.3',
+    ],
+)
