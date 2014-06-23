6aba44a574a5823898ff17bb751b03a51631063d
Cole Bessee <cbessee@uw.edu>
2014-06-23 14:26:32 -0700

    Adds urls, templates, and views for tools app. Forms/tools have to be implemented on live site and under specific slugs. For example closing procedures checklist goes under closing-procedures slug

--- labgeeks_apollo/urls.py
+++ labgeeks_apollo/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import *
 
 urlpatterns = patterns('labgeeks_apollo.views',
-                        url(r'^closing-procedures', 'views.closing_procedures'),
+                        url(r'^closing-procedures/$', 'closing_procedures'),
+                        url(r'^printer-issue/$', 'printer_issue'),
+                        url(r'^login-issue/$', 'login_issue'),
+                        url(r'^$', 'tools_list'),
 )
--- labgeeks_apollo/views.py
+++ labgeeks_apollo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from forms_builder.forms.signal import *
-from forms_builder.forms.form import FormForForm
+from forms_builder.forms.models import *
 
-def closing_procedure(request):
+def closing_procedures(request):
     return render(request, 'closing-procedures.html')
+
+def printer_issue(request):
+    return render_to_response('printer-issue.html', context_instance=RequestContext(request))
+
+def login_issue(reqeust):
+    return render(request, 'login-issue.html')
+
+def tools_list(request):
+    forms = Form.objects.all()
+    params = {'forms': forms, 'request': request}
+    return render_to_response('tools.html', params)
