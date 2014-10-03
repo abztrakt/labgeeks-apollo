5d23629ada7465a562a443cd33d6161698c76bba
Cole Bessee <cbessee@uw.edu>
2014-10-03 10:37:29 -0700

    Adding login requirement to acess for pages

--- labgeeks_apollo/urls.py
+++ labgeeks_apollo/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('labgeeks_apollo.views',
                         url(r'^closing-procedures/$', 'closing_procedures'),
                         url(r'^printer-issue/$', 'printer_issue'),
                         url(r'^login-issue/$', 'login_issue'),
-                        url(r'^$', 'tools_list'),
+                        (r'^$', 'tools_list'),
 )
--- labgeeks_apollo/views.py
+++ labgeeks_apollo/views.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from forms_builder.forms.models import *
 
+@login_required
 def closing_procedures(request):
     return render_to_response('closing-procedures.html', context_instance=RequestContext(request))
 
+@login_required
 def printer_issue(request):
     return render_to_response('printer-issue.html', context_instance=RequestContext(request))
 
+@login_required
 def login_issue(request):
     return render_to_response('login-issue.html', context_instance=RequestContext(request))
 
+@login_required
 def tools_list(request):
     forms = Form.objects.all()
     params = {'forms': forms, 'request': request}
