285fc98d4c121f8fd5f5af932354eb86fc483b55
Cole Bessee <cbessee@uw.edu>
2015-01-16 14:30:30 -0800

    Removing sent template, url route, and view as the default is being overridden in the python package itself

--- labgeeks_apollo/views.py
+++ labgeeks_apollo/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from forms_builder.forms.models import *
+from forms_builder.forms.models import Form
 
 @login_required
 def tool(request, formid):
     form = Form.objects.get(id=formid)
+    form.redirect_url = 'sent/'
     params = {'formid': formid, 'form': form}
     return render_to_response('tool.html', params, context_instance=RequestContext(request))
 
